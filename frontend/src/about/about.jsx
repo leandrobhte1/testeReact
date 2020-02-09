cfba2385d7647c122721a441c46789fcf1049515
Leandro Oliveira <leandrobhte1@hotmail.com>
2020-02-09 14:51:59 -0300

    Initial commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt ipsam, veritatis maxime ab adipisci maiores aspernatur, sed, repellendus dignissimos expedita ullam ut. Ipsum quasi deleniti iusto deserunt sequi distinctio minima.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis necessitatibus dolorem obcaecati reprehenderit temporibus est corporis, accusamus fugit! Harum ullam, impedit ipsum inventore corrupti voluptate mollitia repellendus voluptatibus vitae ea?</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis at, deserunt consectetur excepturi ex consequatur debitis impedit ad numquam ipsa maiores quia aut pariatur cum, rerum perspiciatis fugiat asperiores expedita!</p>
+        
+    </div>
+)
